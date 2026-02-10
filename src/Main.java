794a3a6095afab83e177535836b2eafd9a7c1b1b
khubaib-amjad-alam <119280530+khubaib-amjad-alam@users.noreply.github.com>
2026-02-10 12:29:32 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Master repository");
         System.out.println("updated from eclipse");
+        System.out.println("updated from github remote editor");
         
     }
 }
